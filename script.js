1f6a382aca7be369f9f4d53678bf9690d1127c11
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-19 18:33:53 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+ res.send("WERE HERE!")
+  
+});
+
+app.listen(PORT, () => {
+  console.log("Listending on port " + PORT)
+});
+ 
