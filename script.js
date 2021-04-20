4a3ec2f828b120005ad48031bca4f9c2f9a072f3
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-19 19:35:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
  return res.send("WERE HERE!")
   
 });
+app.post('/api/v1/weatherbyZip', (req, res) => {
+ const api_key = precess.env.OpenWeatherAPIKey;
+ const zip_url = "api.openweathermap.org/data/2.5/weather?zip=";
+ 
+ 
+ return res.send(weatherInfo)
+  
+});
 
 app.listen(PORT, () => {
   console.log("Listending on port " + PORT)
