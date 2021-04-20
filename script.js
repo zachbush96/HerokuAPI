849f88041a1b258d721c29f80d497feeb01d4259
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-19 23:36:17 -0500

    Update script.js
    changing stuff / testing stuff

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/test', (req, res) => {
- res.sendFile('/landing-page/home.html', {root: __dirname });
-});
+app.use(express.static("landing-page"))
 
 app.get('/', (req,res) =>{
  res.send("Youre Here!");
