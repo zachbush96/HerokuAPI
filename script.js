84c49a26c93cb1aa487ea1aaf3399c49743a2ad8
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-19 23:42:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.static("landing-page"))
 
 app.get('/', (req,res) =>{
- res.send("Youre Here!");
+ res.sendFile('index.html', {root: __dirname + '/landing-pages'});
  
 });
 app.post('/api/v1/weatherbyZip', (req, res) => {
