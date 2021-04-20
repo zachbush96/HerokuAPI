f0573f5bfd319fc45f2b1657239e8d6fa3ad9db4
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-20 03:11:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const fetch = require('node-fetch')
 
 const app = express()
 const PORT = process.env.PORT || 3000
-
-async function getData(zipCode){
  
 
 app.use(express.static("landing-page"))
@@ -14,22 +12,22 @@ app.get('/', (req,res) =>{
  res.sendFile('index.html', {root: __dirname + '/landing-pages'});
  
 });
-app.get('/api/v1/weatherbyZip', (req, res) => { 
- if(!req.query.zip){res.send("NO DATA SUMBITTED")}
  
+ app.get('/api/v1/weatherbyZip', (req, res) => { 
+ if(!req.query.zip){res.send("NO DATA SUMBITTED")}
  const api_url = "https://api.openweathermap.org/data/2.5/weather?zip=";
  const appid = "&units=imperial&appid=6ceb02706b07605db27fb252a1bf8521";
  fetch(api_url + res.query.zip + appid)
    .then(res => res.json())
    .then(json => {
       console.log(json);
-      res.send(json)
- }) 
- console.log("THIS DATA -->" + getData(req.query.zip))
- res.send("Is this Getting Sent?")
- });
+      res.send(json);
+  })
+//res.send("Is this Getting Sent?")
+});
+ 
 
 app.listen(PORT, () => {
   console.log("Listending on port " + PORT)
-})
+});
  
