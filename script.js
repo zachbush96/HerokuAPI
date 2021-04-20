95b7464b7b5a43ce210822686c58dbf79d45d8fe
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-20 02:31:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,39 +4,36 @@ const https = require('https')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+async function getData(zipCode){
+ const api_url = "https://api.openweathermap.org/data/2.5/weather?zip=";
+ const appid = "&units=imperial&appid=6ceb02706b07605db27fb252a1bf8521";
+ const options = {
+    hostname: 'api.openweathermap.org',
+    port: 443,
+    path: '/data/2.5/weather?zip=' + zipCode + appid,
+    method: 'GET'
+  }
+  const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+  res.on('data', d => {
+    reaturn d
+  })
+})
+req.on('error', error => {
+  console.error(error)
+})
+req.end()
+
 app.use(express.static("landing-page"))
 
 app.get('/', (req,res) =>{
  res.sendFile('index.html', {root: __dirname + '/landing-pages'});
  
 });
-app.get('/api/v1/weatherbyZip', (req, res) => {
-  weatherData = {} 
- if(!req.query.zip){res.send("NO DATA SUMBITTED")} 
- const api_url = "https://api.openweathermap.org/data/2.5/weather?zip=";
-  const appid = "&units=imperial&appid=6ceb02706b07605db27fb252a1bf8521";
-  const zipCode = req.query.zip
-  const options = {
-    hostname: 'api.openweathermap.org',
-    port: 443,
-    path: '/data/2.5/weather?zip=' + zipCode + appid,
-    method: 'GET'
-  }
-  const req2 = https.request(options, res2 => {
-    console.log(`statusCode: ${res2.statusCode}`)
-    res2.on('data', d => {
-     console.log("This is D: " + d) 
-     res.send(d)
-    })
-  })
-  req2.on('error', error => {
-    res.send("ERROR ENCOUNTERED")
-  })
-  req2.end()
- 
- 
+app.get('/api/v1/weatherbyZip', (req, res) => { 
+ if(!req.query.zip){res.send("NO DATA SUMBITTED")}
+ console.log("THIS DATA -->" + getData(req.query.zip))
  return res.send("Is this Getting Sent?")
-  
 });
 
 app.listen(PORT, () => {
