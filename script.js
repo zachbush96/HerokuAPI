34514d759cb126e781059ea950729b03afd8d5fb
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-20 03:12:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ app.get('/', (req,res) =>{
  if(!req.query.zip){res.send("NO DATA SUMBITTED")}
  const api_url = "https://api.openweathermap.org/data/2.5/weather?zip=";
  const appid = "&units=imperial&appid=6ceb02706b07605db27fb252a1bf8521";
- fetch(api_url + res.query.zip + appid)
+ fetch(api_url + req.query.zip + appid)
    .then(res => res.json())
    .then(json => {
       console.log(json);
